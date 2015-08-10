6f87036679a899eb73cadda6f6e9441f3d0dfb8e
Lukasz Jader <ljaderdev@gmail.com>
2015-08-10 20:04:22 +0200

    Add expectedContentTypes to WSDL; adapt to DataHandler on server endpoint; fix CXF webservice URL

--- test-cxf-base64-ws/src/main/java/pl/ljader/test/cxf/base64/ByteOperationsImpl.java
+++ test-cxf-base64-ws/src/main/java/pl/ljader/test/cxf/base64/ByteOperationsImpl.java
@@ -1,7 +1,11 @@
 package pl.ljader.test.cxf.base64;
 
+import java.io.IOException;
+
 import javax.jws.WebService;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.example.byteoperationsservice.ByteOperationsPortType;
 import org.example.byteoperationsservice.CountBytesRequestType;
 import org.example.byteoperationsservice.CountBytesResponseType;
@@ -11,11 +15,23 @@ serviceName = "ByteOperationsService",
 endpointInterface = "org.example.byteoperationsservice.ByteOperationsPortType",
 wsdlLocation = "wsdl/ByteOperationsService.wsdl")
 public class ByteOperationsImpl implements ByteOperationsPortType {
+	private final Logger LOG = Logger.getLogger(getClass());
 
 	@Override
 	public CountBytesResponseType countBytes(CountBytesRequestType request) {
+		LOG.info("START countBytes");
+		byte[] bytes = null;
+		try {
+			// I don't worry about Java Heap Space exception here, since the client is problem for me
+			bytes = IOUtils.toByteArray(request.getBytesToCount().getInputStream());
+		} catch (IOException ioe) {
+			LOG.warn("error reading bytes to count:", ioe);
+		}
+		int countedSize = bytes.length;
+		LOG.info("STOP countBytes.size:" + countedSize);
+		
 		CountBytesResponseType resp = new CountBytesResponseType();
-		resp.setSizeInBytes(request.getBytesToCount().length);
+		resp.setSizeInBytes(countedSize);
 
 		return resp;
 	}
